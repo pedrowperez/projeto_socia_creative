a37bff0917e19ec83ef2f4feb6353a6867a8e9cc
AndreRusso <andrerussobrito@gmail.com>
2016-06-18 02:00:05 -0300

    Serviços - CSS Final
    divs serv e code dimensionadas em PX

--- js/servicos.js
+++ js/servicos.js
@@ -5,8 +5,8 @@ $(document).ready(function()
 		{
 			if (!move1)
 			{	
-				$(".btn_brad").animate({"margin-top": '0%', "z-index": '1' }, 400);
-				$(".btn_web, .btn_dgraf, .btn_mdig").animate({"margin-top": '70%'}, 400);
+				$(".btn_brad").animate({"margin-top": '40%', "z-index": '1' }, 400);
+				$(".btn_web, .btn_dgraf, .btn_mdig").animate({"margin-top": '150%'}, 400);
 				$('.b1, .b2, .b3').animate({"opacity": '1', "z-index": '1'}, 100);
 				$('.w1, .w2, .w3, .d1, .d2, .d3, .m1, .m2, .m3').animate({"opacity": '0', "z-index": '0'}, 100);
 				$(".btn_brad").css("background", "url(img/servicos_circulo.svg) no-repeat", 500).rotate({
@@ -20,7 +20,7 @@ $(document).ready(function()
 				move4 = false
 			}
 			else{
-				$(".btn_brad").animate({"margin-top": '70%'}, 400);
+				$(".btn_brad").animate({"margin-top": '150%'}, 400);
 				$(".b1, .b2, .b3").animate({"opacity": '0'}, 100);
 				$(".btn_brad").css("background", "url(img/servicos_pentagono.svg) no-repeat", 500).rotate({
       angle: 0,
@@ -36,8 +36,8 @@ $(document).ready(function()
 		{
 			if (!move2)
 			{	
-				$(".btn_web").animate({"margin-top": '0%', "z-index": '1'}, 400);
-				$(".btn_brad, .btn_dgraf, .btn_mdig").animate({"margin-top": '70%'}, 400);
+				$(".btn_web").animate({"margin-top": '40%', "z-index": '1'}, 400);
+				$(".btn_brad, .btn_dgraf, .btn_mdig").animate({"margin-top": '150%'}, 400);
 				$('.w1, .w2, .w3').animate({"opacity": '1', "z-index": '1'}, 100);
 				$('.b1, .b2, .b3, .d1, .d2, .d3, .m1, .m2, .m3').animate({"opacity": '0', "z-index": '0'}, 100);
 				$(".btn_web").css("background", "url(img/servicos_circulo.svg) no-repeat", 500).rotate({
@@ -51,7 +51,7 @@ $(document).ready(function()
 				move4 = false
 			}
 			else{
-				$(".btn_web").animate({"margin-top": '70%'}, 400);
+				$(".btn_web").animate({"margin-top": '150%'}, 400);
 				$(".w1, .w2, .w3").animate({"opacity": '0'}, 100);
 				$(".btn_web").css("background", "url(img/servicos_pentagono.svg) no-repeat", 500).rotate({
       angle: 0,
@@ -67,8 +67,8 @@ $(document).ready(function()
 		{
 			if (!move3)
 			{	
-				$(".btn_dgraf").animate({"margin-top": '0%', "z-index": '1'}, 400);
-				$(".btn_brad, .btn_web, .btn_mdig").animate({"margin-top": '70%'}, 400);
+				$(".btn_dgraf").animate({"margin-top": '40%', "z-index": '1'}, 400);
+				$(".btn_brad, .btn_web, .btn_mdig").animate({"margin-top": '150%'}, 400);
 				$('.d1, .d2, .d3').animate({"opacity": '1', "z-index": '1'}, 100);
 				$('.w1, .w2, .w3, .b1, .b2, .b3, .m1, .m2, .m3').animate({"opacity": '0', "z-index": '0'}, 100);
 				$(".btn_dgraf").css("background", "url(img/servicos_circulo.svg) no-repeat", 500).rotate({
@@ -82,7 +82,7 @@ $(document).ready(function()
 				move4 = false
 			}
 			else{
-				$(".btn_dgraf").animate({"margin-top": '70%'}, 400);
+				$(".btn_dgraf").animate({"margin-top": '150%'}, 400);
 				$(".d1, .d2, .d3").animate({"opacity": '0'}, 100);
 				$(".btn_dgraf").css("background", "url(img/servicos_pentagono.svg) no-repeat", 500).rotate({
       angle: 0,
@@ -98,8 +98,8 @@ $(document).ready(function()
 		{
 			if (!move4)
 			{	
-				$(".btn_mdig").animate({"margin-top": '0%', "z-index": '1'}, 400);
-				$(".btn_brad, .btn_web, .btn_dgraf").animate({"margin-top": '70%'}, 400);
+				$(".btn_mdig").animate({"margin-top": '40%', "z-index": '1'}, 400);
+				$(".btn_brad, .btn_web, .btn_dgraf").animate({"margin-top": '150%'}, 400);
 				$(".m1, .m2, .m3").animate({"opacity": '1', "z-index": '1'}, 100);
 				$('.w1, .w2, .w3, .d1, .d2, .d3, .b1, .b2, .b3').animate({"opacity": '0', "z-index": '0'}, 100);
 				$(".btn_mdig").css("background", "url(img/servicos_circulo.svg) no-repeat", 500).rotate({
@@ -113,7 +113,7 @@ $(document).ready(function()
 				move4 = true
 			}
 			else{
-				$(".btn_mdig").animate({"margin-top": '70%'}, 400);
+				$(".btn_mdig").animate({"margin-top": '150%'}, 400);
 				$(".m1, .m2, .m3").animate({"opacity": '0'}, 100);
 				$(".btn_mdig").css("background", "url(img/servicos_pentagono.svg) no-repeat", 500).rotate({
       angle: 0,
